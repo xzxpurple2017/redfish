4c837be97efab9311d84df7c392b2b5f5844cb66
xzxpurple2017 <philipdam@hotmail.com>
2018-02-05 14:09:24 -0800

    Automate deployment of _redfishobject module

--- vagrant/ubuntu_16.04_redfish/scripts/provision_redfish.sh
+++ vagrant/ubuntu_16.04_redfish/scripts/provision_redfish.sh
@@ -9,5 +9,5 @@ sudo pip3 install python-redfish
 sudo pip3 install python-ilorest-library
 if [[ ! -d /home/vagrant/python-ilorest-library ]] ; then
 	git clone https://github.com/HewlettPackard/python-ilorest-library.git
-	cp /home/vagrant/python-ilorest-library/examples/Redfish/_redfishobject.py /home/vagrant
+	sudo cp /home/vagrant/python-ilorest-library/examples/Redfish/_redfishobject.py /usr/local/lib/python3.5/dist-packages/
 fi
