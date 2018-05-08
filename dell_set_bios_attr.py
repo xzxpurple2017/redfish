7b67fa017e5fb5d40e823aba0d2bdff90967cb66
xzxpurple2017 <philipdam@hotmail.com>
2018-05-08 17:26:40 +0000

    New changes

--- dell_set_bios_attr.py
+++ dell_set_bios_attr.py
@@ -20,34 +20,34 @@ import warnings
 warnings.filterwarnings("ignore")
 
 def parse_args():
-    global iDRAC_https_url
-    global iDRAC_account
-    global iDRAC_password
-    global asset_tag
-    global reboot_flag
-    global data_file
-    global default
+	global iDRAC_https_url
+	global iDRAC_account
+	global iDRAC_password
+	global asset_tag
+	global reboot_flag
+	global data_file
+	global default
 
-    parser = argparse.ArgumentParser(
-        description='Set BIOS attributes for Dell 12th, 13th, and 14th gen servers',
-        formatter_class=argparse.RawTextHelpFormatter
-    )
-    parser.add_argument('-i', '--ip', help='Enter IP address', required=True)
-    parser.add_argument('-a', '--asset', help='Enter asset tag', required=True)
-    parser.add_argument('-u', '--username', help='Enter username', required=True)
-    parser.add_argument('-p', '--password', help='Enter password', required=True)
-    parser.add_argument('-f', '--file', help='Path to JSON config file', required=True)
-    parser.add_argument('-r', '--reboot', help='Toggle to reboot', action='store_true')
-    parser.add_argument('-d', '--default', help='Reset BIOS to factory defaults', action='store_true')
-    args = vars(parser.parse_args())
+	parser = argparse.ArgumentParser(
+		description='Set BIOS attributes for Dell 12th, 13th, and 14th gen servers',
+		formatter_class=argparse.RawTextHelpFormatter
+	)
+	parser.add_argument('-i', '--ip', help='Enter IP address', required=True)
+	parser.add_argument('-a', '--asset', help='Enter asset tag', required=True)
+	parser.add_argument('-u', '--username', help='Enter username', required=True)
+	parser.add_argument('-p', '--password', help='Enter password', required=True)
+	parser.add_argument('-f', '--file', help='Path to JSON config file', required=True)
+	parser.add_argument('-r', '--reboot', help='Toggle to reboot', action='store_true')
+	parser.add_argument('-d', '--default', help='Reset BIOS to factory defaults', action='store_true')
+	args = vars(parser.parse_args())
 
-    iDRAC_https_url = 'https://' + args['ip']
-    iDRAC_account = args['username']
-    iDRAC_password = args['password']
-    asset_tag = args['asset']
-    data_file = args['file']
-    reboot_flag = args['reboot']
-    default = args['default']
+	iDRAC_https_url = 'https://' + args['ip']
+	iDRAC_account = args['username']
+	iDRAC_password = args['password']
+	asset_tag = args['asset']
+	data_file = args['file']
+	reboot_flag = args['reboot']
+	default = args['default']
 
 class Utils:
 	def __init__(self, iDRAC_https_url, iDRAC_account, iDRAC_password):
@@ -223,6 +223,45 @@ class Utils:
 					return ('# ERROR -- Could not set iDRAC password. Returned error code %s') % (response.status_code)
 				break
 	
+		# Get 1Gbps NIC
+		def get_first_one_gbps_nic(self):
+			ethernet_dev_url = "%s/Systems/System.Embedded.1/EthernetInterfaces" % (self.root_url)
+			headers = {
+				'Content-Type': "application/json",
+				'X-Auth-Token': self.x_auth_token
+			}
+			# Determine the 1Gbps interface
+			# We want this to boot second after the hard drive
+			response = requests.request(
+				"GET",
+				ethernet_dev_url,
+				headers=headers,
+				verify=False,
+				timeout=10.000
+			)
+			output = response.json()
+
+			one_gbps_list = []
+			for e in output['Members']:
+				each_nic_path = e['@odata.id']
+				each_nic_url = "%s%s" % (self.iDRAC_https_url, each_nic_path)
+				response = requests.request(
+					"GET",
+					each_nic_url,
+					headers=headers,
+					verify=False,
+					timeout=10.000
+				)
+				output = response.json()
+				if output['SpeedMbps'] == 1000:
+					one_gbps_list.append(output['Id'])
+			try:
+				target_nic = sorted(one_gbps_list)[0]
+			except:
+				target_nic = 'NIC.Integrated.1-1-1'
+
+			return target_nic
+
 	# Dell currently does not support creating RAID virtual disks via Redfish
 	# They plan on releasing this feature Q2 2018
 	# Until then, you can use SCP feature to import these settings
@@ -253,61 +292,95 @@ class Utils:
 	#def get_boot_order(self):
 
 	def set_boot_order(self):
+		get_boot_ord_url = "%s/Systems/System.Embedded.1/BootSources" % (self.root_url)
 		set_boot_ord_url = "%s/Systems/System.Embedded.1/BootSources/Settings" % (self.root_url)
-		ethernet_dev_url = "%s/Systems/System.Embedded.1/EthernetInterfaces" % (self.root_url)
 		headers = {
 			'Content-Type': "application/json",
 			'X-Auth-Token': self.x_auth_token
 		}
-		# Determine the 1Gbps interface
-		# We want this to boot second after the hard drive
+		if self.current_boot_mode == "Uefi":
+			boot_seq = "UefiBootSeq"
+		else:
+			boot_seq = "BootSeq"
+
+		# First, get the available boot devices
+		# Note, this can vary between R640 and R740xd
+		# Then, make sure that hard drive is index 0 and PXE is 1 and all the others are after
+		# Tricky coding below
+		#
 		response = requests.request(
 			"GET",
-			ethernet_dev_url,
+			get_boot_ord_url,
 			headers=headers,
 			verify=False,
 			timeout=10.000
 		)
 		output = response.json()
 
-		one_gbps_list = []
-		for e in output['Members']:
-			each_nic_path = e['@odata.id']
-			each_nic_url = "%s%s" % (self.iDRAC_https_url, each_nic_path)
-			response = requests.request(
-				"GET",
-				each_nic_url,
-				headers=headers,
-				verify=False,
-				timeout=10.000
-			)
-			output = response.json()
-			if output['SpeedMbps'] == 1000:
-				one_gbps_list.append(output['Id'])
-		try:
-			target_nic = sorted(one_gbps_list)[0]
-		except:
-			target_nic = 'NIC.Integrated.1-1-1'
-		
-		# Set the boot order to have hard drive boot first and then NIC
-		if self.current_boot_mode == "Uefi":
-			boot_seq = "UefiBootSeq"
-		else:
-			boot_seq = "BootSeq"
+		payload = {}
+		bootseq_list = []
+
+		index_hash = {}
+		for e in output['Attributes'][boot_seq]:
+			index_hash[e['Index']] ={
+										'Enabled' = e['Enabled'],
+										'Id' = e['Id'],
+										'Name' = e['Name']
+									}
+
+		payload['Attributes'] = {boot_seq:bootseq_list}
+		json_data = json.dumps(payload)
+
+		return json_data
+
+		# Build a hash first and then convert to JSON
+		# Should look something like this for Dell R640
+		#
+		#
+		# "Attributes": {
+		#  	"BootSeq": [
+		# 		{
+		# 			"Enabled": true,
+		# 			"Id": "BIOS.Setup.1-1#BootSeq#HardDisk.List.1-1#c9203080df84781e2ca3d512883dee6f",
+		# 			"Index": 0,
+		# 			"Name": "HardDisk.List.1-1"
+		# 		},
+		# 		{
+		# 			"Enabled": true,
+		# 			"Id": "BIOS.Setup.1-1#BootSeq#NIC.Integrated.1-1-1#4dee2631ee9d41c7976443d88057b88c",
+		# 			"Index": 1,
+		# 			"Name": "NIC.Integrated.1-1-1"
+		# 		}
+		# 	],
+		# 	"HddSeq": [
+		# 		{
+		# 			"Id": "BIOS.Setup.1-1#HddSeq#RAID.Integrated.1-1#c8f9584f29b46cfce3d888826e4305d0",
+		# 			"Index": 0,
+		# 			"Name": "RAID.Integrated.1-1"
+		# 		}
+		# 	]
+		# }
+		#
+		#
+
 
 		#boot_device_list = '[{"Enabled": true, "Id": "BIOS.Setup.1-1#BootSeq#HardDisk.List.1-1#c9203080df84781e2ca3d512883dee6f", "Index": 0, "Name": "HardDisk.List.1-1"},{"Enabled": true, "Id": "BIOS.Setup.1-1#BootSeq#%s#c375e70570f3960b7889246f797540f2", "Index": 1, "Name": "%s"}]' % (target_nic,target_nic)
-		boot_device_list = '[{"Enabled": true, "Id": "BIOS.Setup.1-1#BootSeq#HardDisk.List.1-1", "Index": 0, "Name": "HardDisk.List.1-1"},{"Enabled": true, "Id": "BIOS.Setup.1-1#BootSeq#%s", "Index": 1, "Name": "%s"}]' % (target_nic,target_nic)
-		payload = {"Attributes": {boot_seq:boot_device_list}}
-		return payload
-		response = requests.request(
-			"PATCH",
-			set_boot_ord_url,
-			data=json.dumps(payload),
-			headers=headers,
-			verify=False,
-			timeout=60.000
-		)
-		output = response.json()
+		#boot_device_list = '[{Enabled: true, Id: BIOS.Setup.1-1#BootSeq#HardDisk.List.1-1, Index: 0, Name: HardDisk.List.1-1},{Enabled: true, Id: BIOS.Setup.1-1#BootSeq#%s, Index: 1, Nameuuuuuuu: %s}]' % (target_nic,target_nic)
+		#payload = {"Attributes": {boot_seq:boot_device_list}}
+		#return payload
+		#data=json.dumps(payload)
+		#return data
+
+		#response = requests.request(
+		#	"PATCH",
+		#	set_boot_ord_url,
+		#	#data=json.dumps(payload),
+		#	data=json_data,
+		#	headers=headers,
+		#	verify=False,
+		#	timeout=60.000
+		#)
+		#output = response.json()
 		#return (response.status_code, output)
 
 ##===main program==
